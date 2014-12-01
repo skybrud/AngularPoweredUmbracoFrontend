e96d4e1e74dab7f269932244ea0ec9b9fce1947b
René Pjengaard <rpjengaard@skybrud.dk>
2014-12-01 16:34:42 +0100

    Refactor and cleaning

--- umbraco/code/models/GlobalModel.cs
+++ umbraco/code/models/GlobalModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
-using Umbraco.Core.Models;
+﻿using Umbraco.Core.Models;
 using Umbraco.Web;
 
 namespace code.models
--- umbraco/code/models/helper/ImageModel.cs
+++ umbraco/code/models/helper/ImageModel.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Net.Mime;
-using System.Text;
-using System.Threading.Tasks;
 using System.Web;
 using Newtonsoft.Json;
 using Umbraco.Core.Models;
